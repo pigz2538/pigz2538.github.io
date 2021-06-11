dde09a70545761625ebaa601d9f32bec7e46a30e
pigz2538 <z2281717797@163.com>
2021-06-11 11:29:59 +0800

    Site updated: 2021-06-11 11:29:58

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","3f76c306117191b8d6c918d67f22462e"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
